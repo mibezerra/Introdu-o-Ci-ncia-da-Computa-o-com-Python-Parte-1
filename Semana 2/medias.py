19e548bdecacc9251027f8cae84493aac55a44e2
Camila Bezerra <camilabezerra1996@gmail.com>
2021-10-13 12:13:19 -0300

    Add files via upload

---
+++ Semana 2/medias.py
@@ -0,0 +1,8 @@
+p1 = int(input("Digite o valor da primeira nota: "))
+p2 = int(input("Digite o valor da segunda nota: "))
+p3 = int(input("Digite o valor da terceira nota: "))
+p4 = int(input("Digte o valor da quarta nota: "))
+
+media = (p1 + p2 + p3+ p4)/4
+print("A média aritmética é", media)
+
---
+++ Semana 2/quadrado.py
@@ -0,0 +1,4 @@
+l = int(input("Digite o valor correspondente ao lado de um quadrado: "))
+p = 4 * l
+a = l * l
+print("perímetro:", p, "-", "área:", a)
